83f71cda5b8b8727cc9b792a69a52abe69f91aa8
Agustin Acevedo <agustin.acevedo.13@sansano.usm.cl>
2021-10-03 23:11:53 -0300

    Anadidas metricas mediante plausible selfhosted

--- resources/js/videojs.js
+++ resources/js/videojs.js
@@ -1,11 +1,96 @@
+//videojs modules
 import videojs from "video.js";
 require('@silvermine/videojs-quality-selector')(videojs);
+require('videojs-event-tracking');
 
+//videojs initialization
 const player = videojs('player',{
     fluid: true,
     autoplay: false,
     controls: true,
     playbackRates: [0.5, 1, 1.5, 2, 2.5],
+    plugins: {
+        eventTracking : true,
+    }
 });
 
+//adding qualityselector (relative position 11 for quality selection)
 player.controlBar.addChild('QualitySelector',{},11);
+
+/**
+ * Local dev Plausible
+ */
+import Plausible from "plausible-tracker";
+
+const plausible = Plausible({
+    domain: "localdev.lan",
+    trackLocalhost: true,
+    apiHost: "http://plausible.europa.lan"
+});
+
+var firstTime = true;
+plausible.trackPageview();
+player.on('tracking:firstplay', (e,data)=> {
+        if (firstTime){
+            plausible.trackEvent(
+                'video-play',
+                {
+                    props : {
+                        play : 'first-play'
+                    }
+                }
+            );
+            firstTime = false;
+        }
+    }
+);
+player.on('tracking:pause', () => {
+    plausible.trackEvent(
+        'video-play',
+        {
+            props : {
+                play : 'pause',
+            }
+        }
+    );
+});
+player.on('tracking:first-quarter',()=>{
+    plausible.trackEvent(
+        'video-positioning',
+        {
+            props : {
+                position : 'reach-firstquarter',
+            }
+        }
+    );
+});
+player.on('tracking:second-quarter',()=>{
+    plausible.trackEvent(
+        'video-positioning',
+        {
+            props : {
+                position : 'reach-half',
+            }
+        }
+    );
+});
+player.on('tracking:third-quarter',()=>{
+    plausible.trackEvent(
+        'video-positioning',
+        {
+            props : {
+                position : 'reach-thirdquarter',
+            }
+        }
+    );
+});
+player.on('tracking:fourth-quarter',()=>{
+    plausible.trackEvent(
+        'video-positioning',
+        {
+            props : {
+                position : 'reach-end',
+            }
+        }
+    );
+});
--- resources/views/dev.blade.php
+++ resources/views/dev.blade.php
@@ -14,6 +14,27 @@
         @else
         @endisset
     </div>
+    <!-- en proceso -->
+    <div class="card text-center">
+        <div class="card-header">
+            <nav>
+                <div class="nav nav-tabs card-header-tabs" id="nav-tab" role="tablist">
+                    <a class="nav-link active" id="nav-info-tab" data-toggle="tab" href="#nav-info" role="tab" aria-controls="nav-info" aria-selected="true">Información</a>
+                    @isset($publicacion)
+                        <a class="nav-link" id="nav-video-tab" data-toggle="tab" href="#nav-video" aria-controls="nav-video" aria-selected="false">Video</a>
+                        @can('modevento')
+                            <a class="nav-link" id="nav-share-tab" data-toggle="tab" href="#nav-share" role="tab" aria-controls="nav-share" aria-selected="false">Compartir</a>
+                        @endcan
+                    @endisset
+                </div>
+            </nav>
+        </div>
+        <div class="card-body">
+            <div class="tab-content">
+
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
